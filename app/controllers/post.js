4bdf99f46f03f8772984600f9de410f2508c3304
ikhakoo <ikhakoo@gmail.com>
2015-07-10 17:43:55 -0400

    updating post models and adding partial for editing

---
+++ app/controllers/post.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+var PostController = {
+	isEditing: false,
+	actions: {
+		edit: function () {
+			this.set('isEditing', true);
+		},
+
+		doneEditing: function() {
+			this.set('isEditing', false);
+		}
+	}
+};
+
+export default Ember.ObjectController.extend(PostController);
--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,19 +1,59 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+var posts = [
+  {
+    id: 1,
+    title: 'Bananas',
+    author: 1,
+    date: new Date(2014, 5, 4, 6, 0, 0),
+    body: 'BANANAS'
+  },
+  {
+    id: 2,
+    title: 'Apples',
+    author: 1,
+    date: new Date(2014, 5, 4, 6, 0, 0),
+    body: 'APPLES'
+  }
+];
+
+var authors = [
+  {
+    id: 1,
+    name: 'Imran',
+    posts: [1,2]
+  }
+];
+
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
 
   postsRouter.get('/', function(req, res) {
     res.send({
-      'posts': [
-        {
-          id: 1,
-          title: 'Bananas'
-        },
-        {
-          id: 2,
-          title: 'Apples'
-        }
-      ]
+      "posts": posts,
+      "authors": authors
     });
   });
 
@@ -23,9 +63,10 @@ module.exports = function(app) {
 
   postsRouter.get('/:id', function(req, res) {
     res.send({
-      'posts': {
-        id: req.params.id
-      }
+      "post": posts.find(function(post){
+        return post.id == req.params.id
+      }),
+      "authors": authors
     });
   });
 
